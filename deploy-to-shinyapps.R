5f9e2c7d0c9254168cf331bf86082d4ce7e867f9
Haorui Zhang <ansonzhr@gmail.com>
2019-06-03 17:27:31 -0700

    Deploy to shinyapps

---
+++ deploy-to-shinyapps.R
@@ -0,0 +1,17 @@
+
+# Deploy to shinyapps.io
+# First you will need an account
+
+#install.packages('rsconnect')
+
+# Name is account name, get both your authentication token and secret in your account
+rsconnect::setAccountInfo(name = 'mtsticcc',
+                          token = '<hide>',
+                          secret = '<SECRET>')
+
+setwd("~/Desktop/stat418-final-project")
+library(rsconnect)
+rsconnect::deployApp(appDir = 'docker/app/', appName = "Predicting_Winning_Rate_for_MLB_Teams")
+
+# This is now running at
+# https://mysticcc.shinyapps.io/Predicting_Winning_Rate_for_MLB_Teams/
